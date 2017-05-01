ef29c76b48fa070b0b9793a8379eda79e3368be7
agates4 <agate97@gmail.com>
2017-05-01 01:45:22 -0400

    didnt work

--- touchID/platforms/browser/www/js/touchIDHandler.js
+++
@@ -1,8 +0,0 @@
-
-function isAvailable() {
-     window.plugins.touchid.isAvailable(
-                function() {alert('available!')}, // success handler: TouchID available
-                function(msg) {alert('not available, message: ' + msg)} // error handler: no TouchID available
-            );
-  }
-document.getElementById('touchIDButton').addEventListener('click', isAvailable);
